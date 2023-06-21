00087656e0091cd4711dfd442e33d07bb70823e0
Thomas <thomas.blochet@polyconseil.fr>
2023-06-21 14:16:23 +0200

    init with atlantis bot muter

---
+++ src/atlantis-bot-muter.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name            Atlantis Bot Muter
+// @namespace
+// @version         1.0.0
+// @description     Mute that damn bot
+// @author          toto
+// @run-at          document-end
+// @match           https://gitlab.polyconseil.fr/fdj/si-paiement/-/merge_requests/*
+// ==/UserScript==
+"use strict";
+console.log("Atlantis Bot Muter loaded");
